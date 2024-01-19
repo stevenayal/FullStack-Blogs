81b050735deb81f159ea5d1334ee5e608085bb89
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-01-19 11:15:02 +0530

    .

--- Backend/Helpers/Libraries/imageUpload.js
+++ Backend/Helpers/Libraries/imageUpload.js
@@ -5,33 +5,33 @@ const path = require("path")
 
 const storage = multer.diskStorage({
 
-    destination : function(req,file,cb ){
-        const rootDir = path.dirname(require.main.filename) 
-        
-        if(file.fieldname === "photo"){
-            cb(null , path.join( rootDir,"/public/userPhotos"))
+    destination: function (req, file, cb) {
+        const rootDir = path.dirname(require.main.filename)
+
+        if (file.fieldname === "photo") {
+            cb(null, path.join(rootDir, "/public/userPhotos"))
         }
         else {
-            cb(null , path.join( rootDir,"/public/storyImages"))
+            cb(null, path.join(rootDir, "/public/storyImages"))
         }
 
-    } ,
+    },
 
-    filename :function(req,file,cb ) {
+    filename: function (req, file, cb) {
 
-       
-        if(file.fieldname ==="photo"){
-            const extentions =file.mimetype.split("/")[1]
-           
-            req.savedUserPhoto ="photo_user_" +req.user.id + "."+ extentions
 
-            cb(null ,req.savedUserPhoto)
+        if (file.fieldname === "photo") {
+            const extentions = file.mimetype.split("/")[1]
+
+            req.savedUserPhoto = "photo_user_" + req.user.id + "." + extentions
+
+            cb(null, req.savedUserPhoto)
         }
 
         else {
-            req.savedStoryImage ="image_" +new Date().toISOString().replace(/:/g, '-') + file.originalname 
+            req.savedStoryImage = "image_" + new Date().toISOString().replace(/:/g, '-') + file.originalname
 
-            cb(null ,req.savedStoryImage)
+            cb(null, req.savedStoryImage)
         }
 
     }
@@ -39,19 +39,19 @@ const storage = multer.diskStorage({
 })
 
 
-const fileFilter =(req,file,cb ) => {
+const fileFilter = (req, file, cb) => {
 
-    allowedMimeTypes = ["image/jpeg","image/jpg","image/png","image/gif"]
+    allowedMimeTypes = ["image/jpeg", "image/jpg", "image/png", "image/gif"]
 
     if (!allowedMimeTypes.includes(file.mimetype)) {
-        return (new CustomError("Please provide a valid image file ",400),null )
+        return (new CustomError("Please provide a valid image file ", 400), null)
     }
-    
-    cb(null , true ) ;
-    
+
+    cb(null, true);
+
 }
 
-const imageUpload = multer({storage ,fileFilter  })
+const imageUpload = multer({ storage, fileFilter })
 
 
-module.exports = imageUpload ; 
+module.exports = imageUpload; 
