dcba601f5b8a99e88eeca217003569a806c87893
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-01-27 16:01:59 +0530

    update

--- Frontend/src/components/GeneralScreens/Footer.js
+++ Frontend/src/components/GeneralScreens/Footer.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import '../../Css/Footer.css'
+import React, { useState, useEffect } from 'react';
+import '../../Css/Footer.css';
 
 const Footer = () => {
-    return (
-        <div>
-            <div className="footer">
-            </div>
-            <div className="copyright">
-                <p className="copyright-blog">© 2024 Aditya. All Rights Reserved</p>
-            </div>
-        </div>
-    )
-}
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Update the year every 60 seconds to account for the new year
+      setCurrentYear(new Date().getFullYear());
+    }, 60000);
+
+    return () => clearInterval(interval);
+  }, []); // Run only once on component mount
+
+  return (
+    <div>
+      <div className="footer">
+      </div>
+      <div className="copyright">
+        <p className="copyright-blog">© {currentYear} Aditya. All Rights Reserved</p>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
